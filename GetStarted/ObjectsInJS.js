258587a6e6f9313fcfc4fb8935ac604c46570691
theDTS1 <orel1one@gmail.com>
2022-06-05 16:38:49 +0300

    new commit

---
+++ GetStarted/ObjectsInJS.js
@@ -0,0 +1,12 @@
+//Object Literral syntax
+const circle = {
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1,
+  },
+  // if a dunction is inside here we call it a method
+  draw: function () {
+    console.log("draw!");
+  },
+};
