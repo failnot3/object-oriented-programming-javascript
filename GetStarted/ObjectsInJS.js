789e00eeaabb1b9ee7bb06fe36a03b23f742595c
theDTS1 <orel1one@gmail.com>
2022-06-05 16:50:52 +0300

    Constructor function

--- GetStarted/ObjectsInJS.js
+++ GetStarted/ObjectsInJS.js
@@ -16,3 +16,30 @@ const circle = {
 circle.draw();
 
 // ----------------------------------------------------------
+// Factory Function
+function createCircle() {
+  return {
+    // Inside here we have properties - they hold value
+    radius,
+    // radius: 1,
+    // location: {
+    //   x: 1,
+    //   y: 1,
+    // },
+    draw: function () {
+      console.log("draw!");
+    },
+  };
+}
+const circleCreate = createCircle(1);
+
+// Constructor function
+function Circle(radius) {
+  console.log("this", this);
+  //this is a reference !
+  this.radius = radius;
+  this.draw = function () {
+    console.log("draw");
+  };
+}
+const another = new Circle(1);
--- OOPBasics/FirstPart/index.js
+++ OOPBasics/FirstPart/index.js
@@ -1,16 +1,10 @@
-//Object Literral syntax
-
-const circle = {
-  // Inside here we have properties - they hold value
-  radius: 1,
-  location: {
-    x: 1,
-    y: 1,
-  },
-  // if a dunction is inside here we call it a method - methods hold logic
-  draw: function () {
-    console.log("draw!");
-  },
-};
-
-circle.draw();
+// Constructor function
+function Circle(radius) {
+  console.log("this", this);
+  //this is a reference !
+  this.radius = radius;
+  this.draw = function () {
+    console.log("draw");
+  };
+}
+const another = new Circle(1);
