b083c603ea8a725bd204299a1f55da056956b020
theDTS1 <orel1one@gmail.com>
2022-06-05 18:42:22 +0300

    Addring or removing Properties

--- MoreInfo/AddingOrRemovingProperties.js
+++ MoreInfo/AddingOrRemovingProperties.js
@@ -8,6 +8,17 @@ const circle = new Circle(10);
 
 // user.token = 'asdasdas';
 
-// Let's add new property to Circle
+// Let's add new property to Circle with a dot notation
 circle.location = { x: 1 };
-// Now we have two properies radius and location
+// Now we have two properties radius and location
+//======================
+// Another way to write that is with the help of bracket notation:
+circle["location"] = { x: 2 };
+
+// or for example we can do this:
+const propertyName = "location";
+circle[propertyName] = { x: 3 };
+
+// we can also "delete" a property
+
+delete circle.location; //or we can use: delete circle["location"]
