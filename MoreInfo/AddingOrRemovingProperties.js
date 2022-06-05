88908c6297034a9eace9f64b9e5b074a881df83c
theDTS1 <orel1one@gmail.com>
2022-06-05 18:37:10 +0300

    changes to AddorRem

--- MoreInfo/AddingOrRemovingProperties.js
+++ MoreInfo/AddingOrRemovingProperties.js
@@ -1,13 +1,13 @@
 function Circle(radius) {
-  //   console.log("this", this);
-  //this is a reference !
   this.radius = radius;
   this.draw = function () {
     console.log("draw");
   };
 }
-// If we don't use new it will use the GLOBAL this
-const another = new Circle(1);
-//In JS functions are OBJECTS!
-Circle.call({}, 2);
-Circle.apply({}, [2]);
+const circle = new Circle(10);
+
+// user.token = 'asdasdas';
+
+// Let's add new property to Circle
+circle.location = { x: 1 };
+// Now we have two properies radius and location
