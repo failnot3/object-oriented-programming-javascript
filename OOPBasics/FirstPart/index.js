51d73cd51b76b3f9348e61031bb651f913feec66
theDTS1 <orel1one@gmail.com>
2022-06-05 16:41:26 +0300

    Updates

--- OOPBasics/FirstPart/index.js
+++ OOPBasics/FirstPart/index.js
@@ -1,1 +1,16 @@
-console.log("Hello");
+//Object Literral syntax
+
+const circle = {
+  // Inside here we have properties - they hold value
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1,
+  },
+  // if a dunction is inside here we call it a method - methods hold logic
+  draw: function () {
+    console.log("draw!");
+  },
+};
+
+circle.draw();
