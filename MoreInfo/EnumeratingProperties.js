43d4eb662d52c2d37af31081606e91891d974362
theDTS1 <orel1one@gmail.com>
2022-06-05 18:49:49 +0300

    EnumeratingProperties

--- MoreInfo/EnumeratingProperties.js
+++ MoreInfo/EnumeratingProperties.js
@@ -9,3 +9,11 @@ const circle = new Circle(10);
 for (let key in circle) {
   if (typeof circle[key] !== "function") console.log(key, circle[key]);
 }
+
+const objKeys = Object.keys(circle);
+console.log(objKeys);
+
+// To check if it exists we can:
+if ("radius" in circle) {
+  console.log("Circle has radius");
+}
